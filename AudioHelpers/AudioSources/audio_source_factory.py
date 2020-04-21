04461e346600a4363bbe3c1c6a0ce7fbda11fdd3
dmyurov <yurov.dmitry.a@gmail.com>
2020-04-22 00:34:01 +0300

    fix source bug #1

--- AudioHelpers/AudioSources/audio_source_factory.py
+++ AudioHelpers/AudioSources/audio_source_factory.py
@@ -4,8 +4,8 @@ from AudioHelpers.AudioSources.ytdl_source import YTDLSource
 
 class AudioSourceFactory:
     @staticmethod
-    def provide_source(self, source_type):
-        if source_type is AudioSourceType.SOURCE_UNKNOWN:
-            return None
+    def provide_source(source_type):
         if source_type is AudioSourceType.SOURCE_YOUTUBE:
             return YTDLSource
+        if source_type is AudioSourceType.SOURCE_UNKNOWN:
+            return None
--- AudioHelpers/AudioSources/audio_source_identifier.py
+++ AudioHelpers/AudioSources/audio_source_identifier.py
@@ -3,29 +3,29 @@ from urllib.parse import urlparse
 
 
 class AudioSourceIdentifier:
-    YOUTUBE_DOMAIN_NAMES = ["youtube.com", "youtu.be"]
-    YANDEXMUSIC_DOMAIN_NAMES = ["music.yandex.ru"]
-    BANDCAMP_DOMAIN_NAMES = []
-    SOUNDCLOUD_DOMAIN_NAMES = []
-    SPOTIFY_DOMAIN_NAMES = []
-
     @staticmethod
-    def identify_source(self, search):
+    def identify_source(search):
+
+        YOUTUBE_DOMAIN_NAMES = ["www.youtube.com", "youtu.be"]
+        YANDEXMUSIC_DOMAIN_NAMES = ["music.yandex.ru"]
+        BANDCAMP_DOMAIN_NAMES = []
+        SOUNDCLOUD_DOMAIN_NAMES = []
+        SPOTIFY_DOMAIN_NAMES = []
         parsed_url = urlparse(search)
 
-        if any(parsed_url.netloc in s for s in self.YOUTUBE_DOMAIN_NAMES):
+        if any(parsed_url.netloc in s for s in YOUTUBE_DOMAIN_NAMES):
             return AudioSourceType.SOURCE_YOUTUBE
 
-        if any(parsed_url.netloc in s for s in self.YANDEXMUSIC_DOMAIN_NAMES):
+        if any(parsed_url.netloc in s for s in YANDEXMUSIC_DOMAIN_NAMES):
             return AudioSourceType.SOURCE_YANDEXMUSIC
 
-        if any(parsed_url.netloc in s for s in self.BANDCAMP_DOMAIN_NAMES):
+        if any(parsed_url.netloc in s for s in BANDCAMP_DOMAIN_NAMES):
             return AudioSourceType.SOURCE_BANDCAMP
 
-        if any(parsed_url.netloc in s for s in self.SOUNDCLOUD_DOMAIN_NAMES):
+        if any(parsed_url.netloc in s for s in SOUNDCLOUD_DOMAIN_NAMES):
             return AudioSourceType.SOURCE_SOUNDCLOUD
 
-        if any(parsed_url.netloc in s for s in self.SPOTIFY_DOMAIN_NAMES):
+        if any(parsed_url.netloc in s for s in SPOTIFY_DOMAIN_NAMES):
             return AudioSourceType.SOURCE_SPOTIFY
 
         return AudioSourceType.SOURCE_UNKNOWN
